989a813f0dd0106a541c231e3a1e62fb54f967ec
Aslan <HeyAslan@yandex.ru>
2022-04-15 03:38:37 +0300

    Optimized the code, add base methods with waitings, add docstring

--- tests/test_properties_page.py
+++ tests/test_properties_page.py
@@ -2,6 +2,7 @@ import time
 
 from enums.chemical_elements_enum import ChemicalElementsEnum
 from enums.exception_enum import ExceptionEnum
+from enums.properties_enum import PropertiesEnum
 from page_element_objects.chemical_element_object import ChemicalElementsObject
 from page_element_objects.properties_element_object import PropertiesElementObject
 from page_objects.properties_page_object import PropertiesPageObject
@@ -13,20 +14,20 @@ class TestProperties:
         """Test for checking chlorine discovered"""
         PropertiesPageObject(browser)
         chemical_table = ChemicalElementsObject(browser)
-        chemical_table.click_on_elem(
-            xpath=ChemicalElementsEnum.CHLORINE_ELEM_BTN.value.format(ChemicalElementsEnum.CHLORINE_NUMBER.value["Chlorine"]))
+        chemical_table.click_on_chemical_elem(
+            ChemicalElementsEnum.ELEM_BTN.value.format(ChemicalElementsEnum.ELEM_NUMBER.value["Chlorine"]))
         properties = PropertiesElementObject(browser)
-        chlorine_discovered = properties.get_discovered
-        assert chlorine_discovered == ChemicalElementsEnum.CHLORINE_DISCOVERED_YEAR.value, \
+        chlorine_discovered_year = properties.get_property_value(PropertiesEnum.DISCOVERED_ID.value)
+        assert chlorine_discovered_year == ChemicalElementsEnum.CHLORINE_DISCOVERED_YEAR.value, \
             ExceptionEnum.DISCOVERED_WRONG_EXCEPTION.value  # TODO '\' this symbol does not match PEP8
 
     def test_discovered_url_changed(self, browser):
         """Test for change url after click on chlorine element"""
         PropertiesPageObject(browser)
         chemical_table = ChemicalElementsObject(browser)
-        chemical_table.click_on_elem(
-            xpath=ChemicalElementsEnum.CHLORINE_ELEM_BTN.value.format(ChemicalElementsEnum.CHLORINE_NUMBER.value["Chlorine"]))
+        chemical_table.click_on_chemical_elem(
+            ChemicalElementsEnum.ELEM_BTN.value.format(ChemicalElementsEnum.ELEM_NUMBER.value["Chlorine"]))
         properties = PropertiesElementObject(browser)
-        properties.click_to_discovered()
-        assert browser.current_url == "https://ptable.com/#Properties/Discovered", \
+        properties.click_property(PropertiesEnum.DISCOVERED_ID.value)
+        assert browser.current_url == PropertiesEnum.DISCOVERED_URL.value, \
             ExceptionEnum.URL_WRONG_EXCEPTION.value
