210d3a5b46d09366d9dcc42caa67d546c9f3d710
aslan <baristan1aslan@gmail.com>
2022-10-17 22:07:29 +0200

    Change conftest with init chromedriver

--- conftest.py
+++ conftest.py
@@ -22,12 +22,8 @@ def browser(request):
     service = Service(os.path.abspath("config/chromedriver"))
     if browser_name == "chrome":
         print("\nStart chrome browser for test..")
-        if sys.platform == "darwin":
-            browser = webdriver.Chrome(service=service,
-                                       options=options)
-        else:
-            browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-
+        browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
+                                   options=options)
     elif browser_name == "firefox":
         print("\nStart firefox browser for test..")
         browser = webdriver.Firefox(options=options)
